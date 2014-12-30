43d0e458cc90b30d5e79643885f1ad3930eac56e
Horacio G. de Oro <hgdeoro@gmail.com>
2014-12-30 13:19:17 -0300

    Update sample settings

---
+++ test_project/djmercadopago_test_app_settings.SAMPLE.py
@@ -0,0 +1,5 @@
+DJMERCADOPAGO_CLIENT_ID = 'your-mp-client-id'
+DJMERCADOPAGO_CLIENTE_SECRET = 'your-mp-secret'
+DJMERCADOPAGO_SANDBOX_MODE = True
+DJMERCADOPAGO_CHECKOUT_PREFERENCE_BUILDER = \
+    'full.path.to.checkout.builder.implementation.function'
