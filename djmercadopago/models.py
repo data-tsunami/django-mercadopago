90fbc874808567b991892a6516ebdd7b3b6ecb7e
Horacio G. de Oro <hgdeoro@gmail.com>
2014-12-30 03:25:19 -0300

    Add idea for model

---
+++ djmercadopago/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Checkout(models.Model):
+    # payment_id = models.BigIntegerField(null=True)
+    checkout_preferences = models.TextField()
+    checkout_response = models.TextField()
+    creation_timestamp = models.DateTimeField(auto_now_add=True)
+    last_update_timestamp = models.DateTimeField(auto_now=True)
