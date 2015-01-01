fc164540226b8026a693af51bbd97a40638729f6
Horacio G. de Oro <hgdeoro@gmail.com>
2015-01-01 12:25:53 -0300

    Add unittest

--- djmercadopago/tests.py
+++ djmercadopago/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
 
-# Create your tests here.
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from djmercadopago.services import MercadoPagoService, BackUrlsBuilder,\
+    CheckoutPreferenceResult
+
+
+class BackUrlsBuilderMock(BackUrlsBuilder):
+    def __init__(self):
+        self._success_url = 'https://www.google.com/'
+        self._failure_url = 'https://www.google.com/'
+        self._pending_url = 'https://www.google.com/'
+
+
+class TestMercadoPagoService(TestCase):
+
+    def test(self):
+        service = MercadoPagoService()
+        checkout_result = service.do_checkout('', BackUrlsBuilderMock())
+        assert isinstance(checkout_result, CheckoutPreferenceResult)
+        checkout_result.url
