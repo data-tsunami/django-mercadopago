821d6fb8a3e8b479abcf13a3c0472add84d85b7c
Horacio G. de Oro <hgdeoro@gmail.com>
2015-01-02 15:29:02 -0300

    Add initial migration

---
+++ djmercadopago/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Checkout',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('checkout_preferences', models.TextField()),
+                ('checkout_response', models.TextField()),
+                ('external_reference', models.TextField()),
+                ('creation_timestamp', models.DateTimeField(auto_now_add=True)),
+                ('last_update_timestamp', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
