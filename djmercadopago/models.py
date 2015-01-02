b817d58c85735a74d19372298e65b3d6b6c0b359
Horacio G. de Oro <hgdeoro@gmail.com>
2015-01-02 15:28:51 -0300

    Add field for external_reference

--- djmercadopago/models.py
+++ djmercadopago/models.py
@@ -9,5 +9,6 @@ class Checkout(models.Model):
     # payment_id = models.BigIntegerField(null=True)
     checkout_preferences = models.TextField()
     checkout_response = models.TextField()
+    external_reference = models.TextField()
     creation_timestamp = models.DateTimeField(auto_now_add=True)
     last_update_timestamp = models.DateTimeField(auto_now=True)
