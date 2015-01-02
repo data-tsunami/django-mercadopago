646ce59a219027e17fe759c1c267bb81d8ccdcf5
Horacio G. de Oro <hgdeoro@gmail.com>
2015-01-02 15:28:34 -0300

    Add manage and settings_dev

---
+++ settings_dev.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+INSTALLED_APPS = (
+    'djmercadopago',
+)
+
+SECRET_KEY = '0123456789'
