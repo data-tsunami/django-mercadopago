cc0ce1fc57c0f5e05138b27d4c37b90fe0e3a820
Horacio G. de Oro <hgdeoro@gmail.com>
2015-07-20 11:46:02 -0300

    Adds comments to the sample settings file

--- sample_project/djmercadopago_sample_app_settings.SAMPLE.py
+++ sample_project/djmercadopago_sample_app_settings.SAMPLE.py
@@ -2,6 +2,16 @@
 
 from __future__ import unicode_literals
 
+#
+# The sample_project.settings imports 'djmercadopago_sample_app_settings'.
+#
+# You can copy this file (remove '.SAMPLE' from its name)
+#  to customize the settings of the sample app.
+#
+# The file 'djmercadopago_sample_app_settings.py' is ignored by Git to
+#  avoid uploading your MercadoPago's settings by mistake.
+#
+
 DJMERCADOPAGO = {
     'CLIENT_ID': 'your-mp-client-id',
     'CLIENTE_SECRET': 'your-mp-secret',
