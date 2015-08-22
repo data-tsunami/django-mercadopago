bf44c1dccc4b4d1bc700dcb1b1671fb6858f84d3
Horacio G. de Oro <hgdeoro@gmail.com>
2015-08-22 16:32:57 -0300

    Create almost empty settings, just to be able to run django-admin

---
+++ djmercadopago/settings.py
@@ -0,0 +1,23 @@
+# minimal configuration, just to be able to run django-admin and create migrations
+
+SECRET_KEY = 'please-dont-use-this-settings-file'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'djmercadopago',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
